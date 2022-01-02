08c8cf67a78a479fa72966e61267db98239945b5
Felipe Pereira de Sousa Barbosa <felipaohb2009@hotmail.com>
2022-01-02 18:56:33 -0300

    Desafio5

--- Desafio5/Program.cs
+++ Desafio5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Desafio5
 {
@@ -10,133 +11,141 @@ namespace Desafio5
             List<Pessoa> essasPessoas = new List<Pessoa>();
             List<Cachorro> essesCachorros = new List<Cachorro>();
             List<Moto> essasMotos = new List<Moto>();
+
+            int i = 0;
+            while (true)
             {
-                int i = 0;
-                while(true)
+                i++;
+                Pessoa _pessoa = new Pessoa();
+                Console.Write("Informe o nome da " + i + " pessoa: ");
+                _pessoa.Nome = Convert.ToString(Console.ReadLine());
+                if (_pessoa.Nome == string.Empty)
+
                 {
-                    i++;
-                    Pessoa _pessoa = new Pessoa();
-                    Console.Write("Informe o nome da " + i + " pessoa: ");
-                    _pessoa.Nome = Convert.ToString(Console.ReadLine());
-                    if(_pessoa.Nome == string.Empty)
-                    
-                    {
-                        break;
-                    }
-                    Console.Write("Informe a data de nascimento da " + i + " pessoa: ");
-                    _pessoa.DataDeNascimento = Convert.ToString(Console.ReadLine());
-                    if (_pessoa.DataDeNascimento == string.Empty)
+                    break;
+                }
+                Console.Write("Informe a data de nascimento da " + i + " pessoa: ");
+                _pessoa.DataDeNascimento = Convert.ToString(Console.ReadLine());
+                if (_pessoa.DataDeNascimento == string.Empty)
 
-                    {
-                        break;
-                    }
-                    Console.Write("Informe a altura da " + i + " pessoa:  ");
-                    _pessoa.Altura = Convert.ToString(Console.ReadLine());
-                    if (_pessoa.Altura == string.Empty)
+                {
+                    break;
+                }
+                Console.Write("Informe a altura da " + i + " pessoa:  ");
+                _pessoa.Altura = Convert.ToString(Console.ReadLine());
+                if (_pessoa.Altura == string.Empty)
 
-                    {
-                        break;
-                    }
-                    Console.Write("Informe o cpf da " + i + " pessoa: ");
-                    _pessoa.CPF = Convert.ToString(Console.ReadLine());
-                    if (_pessoa.CPF == string.Empty)
+                {
+                    break;
+                }
+                Console.Write("Informe o cpf da " + i + " pessoa: ");
+                _pessoa.CPF = Convert.ToString(Console.ReadLine());
+                if (_pessoa.CPF == string.Empty)
 
-                    {
-                        break;
-                    }
-                    Console.Write("Informe o estado civil da " + i + " pessoa: ");
-                    _pessoa.EstadoCivil = Convert.ToString(Console.ReadLine());
-                    if (_pessoa.EstadoCivil == string.Empty)
+                {
+                    break;
+                }
+                Console.Write("Informe o estado civil da " + i + " pessoa: ");
+                _pessoa.EstadoCivil = Convert.ToString(Console.ReadLine());
+                if (_pessoa.EstadoCivil == string.Empty)
 
-                    {
-                        break;
-                    }
+                {
+                    break;
+                }
 
-                    essasPessoas.Add(_pessoa);
+                essasPessoas.Add(_pessoa);
 
-                    Cachorro _cachorro = new Cachorro();
-                    Console.Write("Informe a cor do " + i + " cachorro: ");
-                    _cachorro.Cor = Convert.ToString(Console.ReadLine());
-                    if (_cachorro.Cor == string.Empty)
+                Cachorro _cachorro = new Cachorro();
+                Console.Write("Informe a cor do " + i + " cachorro: ");
+                _cachorro.Cor = Convert.ToString(Console.ReadLine());
+                if (_cachorro.Cor == string.Empty)
 
-                    {
-                        break;
-                    }
-                    Console.Write("Informe a data de nascimento do " + i + " cachorro: ");
-                    _cachorro.DataDeNascimento = Convert.ToString(Console.ReadLine());
-                    if (_cachorro.DataDeNascimento == string.Empty)
+                {
+                    break;
+                }
+                Console.Write("Informe a data de nascimento do " + i + " cachorro: ");
+                _cachorro.DataDeNascimento = Convert.ToString(Console.ReadLine());
+                if (_cachorro.DataDeNascimento == string.Empty)
 
-                    {
-                        break;
-                    }
-                    Console.Write("Informe a cor dos olhos do " + i + " cachorro: ");
-                    _cachorro.CorDosOlhos = Convert.ToString(Console.ReadLine());
-                    if (_cachorro.CorDosOlhos == string.Empty)
+                {
+                    break;
+                }
+                Console.Write("Informe a cor dos olhos do " + i + " cachorro: ");
+                _cachorro.CorDosOlhos = Convert.ToString(Console.ReadLine());
+                if (_cachorro.CorDosOlhos == string.Empty)
 
-                    {
-                        break;
-                    }
-                    Console.Write("Informe o porte do " + i + " cachorro: ");
-                    _cachorro.Porte = Convert.ToString(Console.ReadLine());
-                    if (_cachorro.Porte == string.Empty)
+                {
+                    break;
+                }
+                Console.Write("Informe o porte do " + i + " cachorro: ");
+                _cachorro.Porte = Convert.ToString(Console.ReadLine());
+                if (_cachorro.Porte == string.Empty)
 
-                    {
-                        break;
-                    }
+                {
+                    break;
+                }
 
-                    essesCachorros.Add(_cachorro);
+                essesCachorros.Add(_cachorro);
 
-                    Moto _moto = new Moto();
-                    Console.Write("Informe a Cor da " + i + " moto: ");
-                    _moto.Cor = Convert.ToString(Console.ReadLine());
-                    if (_moto.Cor == string.Empty)
+                Moto _moto = new Moto();
+                Console.Write("Informe a Cor da " + i + " moto: ");
+                _moto.Cor = Convert.ToString(Console.ReadLine());
+                if (_moto.Cor == string.Empty)
 
-                    {
-                        break;
-                    }
-                    Console.Write("Informe o combustivel da " + i + " moto: ");
-                    _moto.Combustivel = Convert.ToString(Console.ReadLine());
-                    if (_moto.Combustivel == string.Empty)
+                {
+                    break;
+                }
+                Console.Write("Informe o combustivel da " + i + " moto: ");
+                _moto.Combustivel = Convert.ToString(Console.ReadLine());
+                if (_moto.Combustivel == string.Empty)
 
-                    {
-                        break;
-                    }
-                    Console.Write("Informe a cilindrada da " + i + " moto: ");
-                    _moto.Cilindrada = Convert.ToString(Console.ReadLine());
-                    if (_moto.Cilindrada == string.Empty)
+                {
+                    break;
+                }
+                Console.Write("Informe a cilindrada da " + i + " moto: ");
+                _moto.Cilindrada = Convert.ToString(Console.ReadLine());
+                if (_moto.Cilindrada == string.Empty)
 
-                    {
-                        break;
-                    }
-                    Console.Write("Informe o modelo da " + i + " moto: ");
-                    _moto.Modelo = Convert.ToString(Console.ReadLine());
-                    if (_moto.Modelo == string.Empty)
+                {
+                    break;
+                }
+                Console.Write("Informe o modelo da " + i + " moto: ");
+                _moto.Modelo = Convert.ToString(Console.ReadLine());
+                if (_moto.Modelo == string.Empty)
 
-                    {
-                        break;
-                    }
+                {
+                    break;
+                }
 
-                    essasMotos.Add(_moto);
+                essasMotos.Add(_moto);
+            }          
 
+                foreach (Pessoa _pessoa in essasPessoas.OrderBy(n => n.Nome))
+                {
                     Console.WriteLine(_pessoa.Nome);
                     Console.WriteLine(_pessoa.DataDeNascimento);
                     Console.WriteLine(_pessoa.Altura);
                     Console.WriteLine(_pessoa.CPF);
                     Console.WriteLine(_pessoa.EstadoCivil);
-
+                }
+                foreach (Cachorro _cachorro in essesCachorros.OrderBy(n => n.Cor))
+                {
                     Console.WriteLine(_cachorro.Cor);
                     Console.WriteLine(_cachorro.DataDeNascimento);
                     Console.WriteLine(_cachorro.CorDosOlhos);
                     Console.WriteLine(_cachorro.Porte);
-
+                }
+                foreach (Moto _moto in essasMotos.OrderBy(n => n.Cor))
+                {
                     Console.WriteLine(_moto.Cor);
                     Console.WriteLine(_moto.Combustivel);
                     Console.WriteLine(_moto.Cilindrada);
                     Console.WriteLine(_moto.Modelo);
-
-                    Console.ReadKey();
                 }
-            }
+            
+            Console.ReadKey();
+
         }
     }
 }
+
